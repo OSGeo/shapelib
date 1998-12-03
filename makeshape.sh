76f68f64bb1151a256b0180aea0b557e66a6c695
Frank Warmerdam <warmerdam@pobox.com>
1998-12-03 16:36:54 +0000

    added test of integer and double attributes as well.

--- makeshape.sh
+++ makeshape.sh
@@ -6,12 +6,13 @@
 #
 
 shpcreate test polygon
-dbfcreate test.dbf -s Description 30
+dbfcreate test.dbf -s Description 30 -n TestInt 6 0 -n TestDouble 16 5
 
 shpadd test 0 0 100 0 100 100 0 100 0 0 + 20 20 20 30 30 30 20 20
-dbfadd test.dbf "Square with triangle missing"
+dbfadd test.dbf "Square with triangle missing" 1.5 2.5
 
 shpadd test 150 150 160 150 180 170 150 150
-dbfadd test.dbf "Smaller triangle"
+dbfadd test.dbf "Smaller triangle" 100 1000.25
+
 
 
