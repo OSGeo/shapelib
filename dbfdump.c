8bde3fd22cd566474639b7f00ec731270be9ed35
Frank Warmerdam <warmerdam@pobox.com>
1998-12-31 15:30:13 +0000

    Added -m, -r, and -h commandline options.

--- dbfdump.c
+++ dbfdump.c
@@ -4,7 +4,10 @@
  * This code is in the public domain.
  *
  * $Log$
- * Revision 1.3  1995-10-21 03:15:01  warmerda
+ * Revision 1.4  1998-12-31 15:30:13  warmerda
+ * Added -m, -r, and -h commandline options.
+ *
+ * Revision 1.3  1995/10/21 03:15:01  warmerda
  * Changed to use binary file access.
  *
  * Revision 1.2  1995/08/04  03:16:22  warmerda
@@ -22,22 +25,44 @@ int main( int argc, char ** argv )
 {
     DBFHandle	hDBF;
     int		*panWidth, i, iRecord;
-    char	szFormat[32], szField[1024];
+    char	szFormat[32], szField[1024], *pszFilename = NULL;
     int		nWidth, nDecimals;
+    int		bHeader = 0;
+    int		bRaw = 0;
+    int		bMultiLine = 0;
+    char	szTitle[12];
+
+/* -------------------------------------------------------------------- */
+/*      Handle arguments.                                               */
+/* -------------------------------------------------------------------- */
+    for( i = 1; i < argc; i++ )
+    {
+        if( strcmp(argv[i],"-h") == 0 )
+            bHeader = 1;
+        else if( strcmp(argv[i],"-r") == 0 )
+            bRaw = 1;
+        else if( strcmp(argv[i],"-m") == 0 )
+            bMultiLine = 1;
+        else
+            pszFilename = argv[i];
+    }
 
 /* -------------------------------------------------------------------- */
 /*      Display a usage message.                                        */
 /* -------------------------------------------------------------------- */
-    if( argc != 2 )
+    if( pszFilename == NULL )
     {
-	printf( "dbfdump xbase_file\n" );
+	printf( "dbfdump [-h] [-r] [-m] xbase_file\n" );
+        printf( "        -h: Write header info (field descriptions)\n" );
+        printf( "        -r: Write raw field info, numeric values not reformatted\n" );
+        printf( "        -m: Multiline, one line per field.\n" );
 	exit( 1 );
     }
 
 /* -------------------------------------------------------------------- */
 /*      Open the file.                                                  */
 /* -------------------------------------------------------------------- */
-    hDBF = DBFOpen( argv[1], "rb" );
+    hDBF = DBFOpen( pszFilename, "rb" );
     if( hDBF == NULL )
     {
 	printf( "DBFOpen(%s,\"r\") failed.\n", argv[1] );
@@ -54,15 +79,39 @@ int main( int argc, char ** argv )
     }
 
 /* -------------------------------------------------------------------- */
+/*	Dump header definitions.					*/
+/* -------------------------------------------------------------------- */
+    if( bHeader )
+    {
+        for( i = 0; i < DBFGetFieldCount(hDBF); i++ )
+        {
+            DBFFieldType	eType;
+            const char	 	*pszTypeName;
+
+            eType = DBFGetFieldInfo( hDBF, i, szTitle, &nWidth, &nDecimals );
+            if( eType == FTString )
+                pszTypeName = "String";
+            else if( eType == FTInteger )
+                pszTypeName = "Integer";
+            else if( eType == FTDouble )
+                pszTypeName = "Double";
+            else if( eType == FTInvalid )
+                pszTypeName = "Invalid";
+
+            printf( "Field %d: Type=%s, Title=`%s', Width=%d, Decimals=%d\n",
+                    i, pszTypeName, szTitle, nWidth, nDecimals );
+        }
+    }
+
+/* -------------------------------------------------------------------- */
 /*	Compute offsets to use when printing each of the field 		*/
 /*	values. We make each field as wide as the field title+1, or 	*/
 /*	the field value + 1. 						*/
 /* -------------------------------------------------------------------- */
     panWidth = (int *) malloc( DBFGetFieldCount( hDBF ) * sizeof(int) );
 
-    for( i = 0; i < DBFGetFieldCount(hDBF); i++ )
+    for( i = 0; i < DBFGetFieldCount(hDBF) && !bMultiLine; i++ )
     {
-	char		szTitle[12];
 	DBFFieldType	eType;
 
 	eType = DBFGetFieldInfo( hDBF, i, szTitle, &nWidth, &nDecimals );
@@ -84,42 +133,70 @@ int main( int argc, char ** argv )
 /* -------------------------------------------------------------------- */
     for( iRecord = 0; iRecord < DBFGetRecordCount(hDBF); iRecord++ )
     {
+        if( bMultiLine )
+            printf( "Record: %d\n", iRecord );
+        
 	for( i = 0; i < DBFGetFieldCount(hDBF); i++ )
 	{
+            DBFFieldType	eType;
+            
+            eType = DBFGetFieldInfo( hDBF, i, szTitle, &nWidth, &nDecimals );
+
+            if( bMultiLine )
+            {
+                printf( "%s: ", szTitle );
+            }
+            
 /* -------------------------------------------------------------------- */
 /*      Print the record according to the type and formatting           */
 /*      information implicit in the DBF field description.              */
 /* -------------------------------------------------------------------- */
-	    switch( DBFGetFieldInfo( hDBF, i, NULL, &nWidth, &nDecimals ) )
-	    {
-	      case FTString:
-		sprintf( szFormat, "%-%%ds ", nWidth );
-		printf( szFormat, 
-		        DBFReadStringAttribute( hDBF, iRecord, i ) );
-		break;
+            if( !bRaw )
+            {
+                switch( eType )
+                {
+                  case FTString:
+                    sprintf( szFormat, "%%-%ds", nWidth );
+                    printf( szFormat, 
+                            DBFReadStringAttribute( hDBF, iRecord, i ) );
+                    break;
+                    
+                  case FTInteger:
+                    sprintf( szFormat, "%%%dd", nWidth );
+                    printf( szFormat, 
+                            DBFReadIntegerAttribute( hDBF, iRecord, i ) );
+                    break;
 
-	      case FTInteger:
-		sprintf( szFormat, "%%%dd ", nWidth );
-		printf( szFormat, 
-		        DBFReadIntegerAttribute( hDBF, iRecord, i ) );
-		break;
+                  case FTDouble:
+                    sprintf( szFormat, "%%%d.%dlf", nWidth, nDecimals );
+                    printf( szFormat, 
+                            DBFReadDoubleAttribute( hDBF, iRecord, i ) );
+                    break;
+                }
+            }
 
-	      case FTDouble:
-		sprintf( szFormat, "%%%d.%dlf ", nWidth, nDecimals );
-		printf( szFormat, 
-		        DBFReadDoubleAttribute( hDBF, iRecord, i ) );
-		break;
-	    }
+/* -------------------------------------------------------------------- */
+/*      Just dump in raw form (as formatted in the file).               */
+/* -------------------------------------------------------------------- */
+            else
+            {
+                sprintf( szFormat, "%%-%ds", nWidth );
+                printf( szFormat, 
+                        DBFReadStringAttribute( hDBF, iRecord, i ) );
+            }
 
 /* -------------------------------------------------------------------- */
 /*      Write out any extra spaces required to pad out the field        */
 /*      width.                                                          */
 /* -------------------------------------------------------------------- */
-	    if( panWidth[i] > nWidth )
+	    if( !bMultiLine )
 	    {
-		sprintf( szFormat, "%%%ds", panWidth[i] - nWidth );
+		sprintf( szFormat, "%%%ds", panWidth[i] - nWidth + 1 );
 		printf( szFormat, "" );
 	    }
+
+            if( bMultiLine )
+                printf( "\n" );
 
 	    fflush( stdout );
 	}
