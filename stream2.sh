941280ede3113b6eeaa4b19f19531934fe547078
Frank Warmerdam <warmerdam@pobox.com>
2000-03-17 14:00:59 +0000

    Use ./ to find tools.

--- stream2.sh
+++ stream2.sh
@@ -6,6 +6,6 @@ for i in 1 2 3 4 5 6 7 8 9 10 11 12 13; do
   echo -----------------------------------------------------------------------
 
   ./shptest $i
-  shpdump test${i}.shp
+  ./shpdump test${i}.shp
 done
 
