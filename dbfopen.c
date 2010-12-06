0346f71f90b46ec9f3b5da945554f0dc5b55588f
Frank Warmerdam <warmerdam@pobox.com>
2010-12-06 16:09:34 +0000

    fix buffer read overrun fetching code page (bug 2276)

--- dbfopen.c
+++ dbfopen.c
@@ -34,6 +34,9 @@
  ******************************************************************************
  *
  * $Log$
+ * Revision 1.85  2010-12-06 16:09:34  fwarmerdam
+ * fix buffer read overrun fetching code page (bug 2276)
+ *
  * Revision 1.84  2009-10-29 19:59:48  fwarmerdam
  * avoid crash on truncated header (gdal #3093)
  *
@@ -492,7 +495,7 @@ DBFOpenLL( const char * pszFilename, const char * pszAccess, SAHooks *psHooks )
     {
         size_t n;
         char *buffer = (char *) pabyBuf;
-        buffer[0] = '\0';
+        memset( buffer, 0, nBufSize);
         psDBF->sHooks.FRead( pabyBuf, nBufSize - 1, 1, pfCPG );
         n = strcspn( (char *) pabyBuf, "\n\r" );
         if( n > 0 )
