65bf1ab1cefc85422300ceca81ac073f9b128df0
Kurt Schwehr <schwehr@google.com>
2021-02-17 07:18:19 -0800

    Cleanup dbfcreate.c
    Trying out a more compact format
    
    - Remove $Id$
    - /* */ multiline comments to single line //
    - Fitting to 100 character lines
    - exit -> return from main as it was mixed before
    - Use variables to for parameters to DBFAddField
    - i = i + # -> i += #
    - Add DBFClose before error returns from main

--- dbfcreate.c
+++ dbfcreate.c
@@ -1,5 +1,4 @@
 /******************************************************************************
- * $Id$
  *
  * Project:  Shapelib
  * Purpose:  Sample application for creating a new .dbf file.
@@ -42,62 +41,50 @@
 
 SHP_CVSID("$Id$")
 
-int main( int argc, char ** argv ) {
-/* -------------------------------------------------------------------- */
-/*      Display a usage message.                                        */
-/* -------------------------------------------------------------------- */
-    if( argc < 2 )
-    {
-	printf( "dbfcreate xbase_file [[-s field_name width],[-n field_name width decimals]]...\n" );
-
-	exit( 1 );
+int main(int argc, char ** argv) {
+    // Display a usage message.
+    if( argc < 2 ) {
+        printf("dbfcreate xbase_file [[-s field_name width], "
+               "[-n field_name width decimals]]...\n");
+        return 1;
     }
 
-/* -------------------------------------------------------------------- */
-/*	Create the database.						*/
-/* -------------------------------------------------------------------- */
+    // Create the database.
     DBFHandle hDBF = DBFCreate( argv[1] );
-    if( hDBF == NULL )
-    {
-	printf( "DBFCreate(%s) failed.\n", argv[1] );
-	exit( 2 );
+    if( hDBF == NULL ) {
+        printf("DBFCreate(%s) failed.\n", argv[1]);
+        return 2;
     }
 
-/* -------------------------------------------------------------------- */
-/*	Loop over the field definitions adding new fields.	       	*/
-/* -------------------------------------------------------------------- */
-    for( int i = 2; i < argc; i++ )
-    {
-	if( strcmp(argv[i],"-s") == 0 && i < argc-2 )
-	{
-	    if( DBFAddField( hDBF, argv[i+1], FTString, atoi(argv[i+2]), 0 )
-                == -1 )
-	    {
-		printf( "DBFAddField(%s,FTString,%d,0) failed.\n",
-		        argv[i+1], atoi(argv[i+2]) );
-		exit( 4 );
-	    }
-	    i = i + 2;
-	}
-	else if( strcmp(argv[i],"-n") == 0 && i < argc-3 )
-	{
-	    if( DBFAddField( hDBF, argv[i+1], FTDouble, atoi(argv[i+2]),
-			      atoi(argv[i+3]) ) == -1 )
-	    {
-		printf( "DBFAddField(%s,FTDouble,%d,%d) failed.\n",
-		        argv[i+1], atoi(argv[i+2]), atoi(argv[i+3]) );
-		exit( 4 );
-	    }
-	    i = i + 3;
-	}
-	else
-	{
-	    printf( "Argument incomplete, or unrecognised:%s\n", argv[i] );
-	    exit( 3 );
-	}
+    // Loop over the field definitions adding new fields.
+    for( int i = 2; i < argc; i++ ) {
+        if(i < argc - 2 && strcmp(argv[i], "-s") == 0) {
+            const char *field = argv[i+1];
+            const int width = atoi(argv[i+2]);
+            const int decimals = 0;
+            if(DBFAddField(hDBF, field, FTString, width, decimals) == -1) {
+                printf("DBFAddField(%s,FTString,%d,0) failed.\n", field, width);
+                DBFClose(hDBF);
+                return 4;
+            }
+            i += 2;
+        } else if(i < argc - 3 && strcmp(argv[i], "-n") == 0) {
+            const char *field = argv[i+1];
+            const int width = atoi(argv[i+2]);
+            const int decimals = atoi(argv[i+3]);
+            if(DBFAddField(hDBF, field, FTDouble, width, decimals) == -1) {
+                printf("DBFAddField(%s,FTDouble,%d,%d) failed.\n", field, width, decimals);
+                DBFClose(hDBF);
+                return 4;
+            }
+            i += 3;
+        } else {
+            printf("Argument incomplete, or unrecognised: %s\n", argv[i]);
+            DBFClose(hDBF);
+            return 3;
+        }
     }
 
-    DBFClose( hDBF );
-
-    return( 0 );
+    DBFClose(hDBF);
+    return 0;
 }
