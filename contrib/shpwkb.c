46df0e357d426441025bc911cd85709faf86b229
Kurt Schwehr <schwehr@google.com>
2021-03-03 12:13:08 -0800

    shpwkb.c: Cleanup
    - This code is still really rough and needs more error checking
    - Free resources on error opening files
    - exit 1 -> return EXIT_FAILURE
    - Remove unused DBFRow variable
    - Cleanup formatting
    - Don't print an extra space before newline

--- contrib/shpwkb.c
+++ contrib/shpwkb.c
@@ -30,33 +30,41 @@
 #include "shapefil.h"
 #include "shpgeo.h"
 
-int main( int argc, char ** argv ) {
-    if( argc < 3 )
-    {
-	printf( "shpwkb shp_file wkb_file\n" );
-	exit( 1 );
+int main(int argc, char ** argv ) {
+    if(argc < 3) {
+        printf( "shpwkb shp_file wkb_file\n" );
+        return EXIT_FAILURE;
     }
 
-    SHPHandle old_SHP = SHPOpen (argv[1], "rb" );
     DBFHandle old_DBF = DBFOpen (argv[1], "rb");
-    if( old_SHP == NULL || old_DBF == NULL )
-    {
-	printf( "Unable to open old files:%s\n", argv[1] );
-	exit( 1 );
+    if(old_DBF == NULL) {
+        printf("Unable to open old dbf: %s\n", argv[1]);
+        return EXIT_FAILURE;
+    }
+    SHPHandle old_SHP = SHPOpen (argv[1], "rb");
+    if(old_SHP == NULL) {
+        printf("Unable to open old shp: %s\n", argv[1]);
+        DBFClose(old_DBF);
+        return EXIT_FAILURE;
     }
 
-    FILE *wkb_file = fopen ( argv[2], "wb");
-    WKBStreamObj *wkbObj = calloc ( 3, sizeof (int) );
+    FILE *wkb_file = fopen(argv[2], "wb");
+    if(wkb_file == NULL) {
+        printf("Unable to open wkb_file: %s\n", argv[2]);
+        DBFClose(old_DBF);
+        SHPClose(old_SHP);
+        return EXIT_FAILURE;
+    }
+
+    WKBStreamObj *wkbObj = calloc(3, sizeof (int));
 
     int nEntities;
     int nShapeType;
-    SHPGetInfo( old_SHP, &nEntities, &nShapeType, NULL, NULL );
+    SHPGetInfo(old_SHP, &nEntities, &nShapeType, NULL, NULL);
 
-    char *DBFRow = NULL;
     int byRing = 0;
 
-    for( int i = 0; i < nEntities; i++ )
-    {
+    for( int i = 0; i < nEntities; i++) {
         SHPObject *psCShape = SHPReadObject( old_SHP, i );
 
         if ( byRing == 1 ) {
@@ -64,31 +72,35 @@ int main( int argc, char ** argv ) {
           for ( int ring = (psCShape->nParts - 1); ring >= 0; ring-- ) {
             const int rStart = psCShape->panPartStart[ring];
 	    int numVtx;
-            if ( ring == (psCShape->nParts -1) )
-              { numVtx = psCShape->nVertices - rStart; }
-             else
-              { numVtx = psCShape->panPartStart[ring+1] - rStart; }
+            if (ring == psCShape->nParts - 1) {
+                numVtx = psCShape->nVertices - rStart;
+            } else {
+                numVtx = psCShape->panPartStart[ring+1] - rStart;
+            }
 
-            printf ("(shpdata) Ring(%d) (%d for %d) \n", ring, rStart, numVtx);
-	    SHPObject *psO = SHPClone ( psCShape, ring,  ring + 1 );
+            printf("(shpdata) Ring(%d) (%d for %d)\n", ring, rStart, numVtx);
+	    SHPObject *psO = SHPClone(psCShape, ring,  ring + 1);
 
-	    SHPDestroyObject ( psO );
-            printf ("(shpdata) End Ring \n");
-           }  /* (ring) [0,nParts  */
+	    SHPDestroyObject (psO);
+            printf ("(shpdata) End Ring\n");
+           }  // (ring) [0,nParts
+        }  // by ring
 
-          }  /* by ring   */
+        printf ("gonna build a wkb\n");
+        // const int res =
+        SHPWriteOGisWKB(wkbObj, psCShape);
+        printf("gonna write a wkb that is %d bytes long\n", wkbObj->StreamPos );
+        fwrite((void*)wkbObj->wStream, 1, wkbObj->StreamPos, wkb_file);
 
-	   printf ("gonna build a wkb \n");
-	   // const int res =
-           SHPWriteOGisWKB ( wkbObj, psCShape );
-	   printf ("gonna write a wkb that is %d bytes long \n", wkbObj->StreamPos );
-	   fwrite ( (void*) wkbObj->wStream, 1, wkbObj->StreamPos, wkb_file );
+        SHPDestroyObject(psCShape);
     }
 
-    free ( wkbObj );
-    SHPClose( old_SHP );
-    DBFClose( old_DBF );
-    if ( wkb_file )  fclose ( wkb_file );
+    free(wkbObj);
+    SHPClose(old_SHP);
+    DBFClose(old_DBF);
+    fclose(wkb_file);
 
     printf ("\n");
+
+    return EXIT_SUCCESS;
 }
